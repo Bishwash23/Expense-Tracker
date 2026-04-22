123ad218edf55b03e193d2c9e93e9ed1d4e25388
Bishwash23 <bishwashb23@gmail.com>
2026-04-22 14:04:17 +0545

    Refactor category management and implement expense addition functionality

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 EXPENSE_FILE = "expense.json"
 CATEGORY_FILE = "category.json"
@@ -14,12 +15,32 @@ def load(f):
     except FileNotFoundError:
         return []
 
-def add_category():
-    category = input("Enter category: ")
+def add_category(category):
     data = load(CATEGORY_FILE)
     if any(item for item in data if item == category):
         print(f"{category} category already exist")
         return
     data.append(category)
     save(data, CATEGORY_FILE)
+
+def add_expense():
+    category_data = load(CATEGORY_FILE)
+    print("Chose one category or create one")
+    for c in category_data:
+        print(c)
+    category = input("Category: ").lower().strip()
+    if not category in category_data:
+        add_category(category)
+    while True:
+        try:
+            amount = float(input("Amount: "))
+            break
+        except ValueError:
+            print("Invalid amount")
+    date = datetime.now().strftime("%Y-%m-%d")
+    time = datetime.now().strftime("%H:%M")
+    data = load(EXPENSE_FILE)
+    data.append({"category":category, "amount":amount, "date":date, "time":time})
+    save(data, EXPENSE_FILE)
+
 
