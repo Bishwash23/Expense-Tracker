188c3cdacdeffa64865712092c8062a41ecb011b
Bishwash23 <bishwashb23@gmail.com>
2026-04-22 14:33:45 +0545

    Enhance user interface with improved prompts and add category summary functionality

--- expense_tracker.py
+++ expense_tracker.py
@@ -25,7 +25,7 @@ def add_category(category):
 
 def add_expense():
     category_data = load(CATEGORY_FILE)
-    print("Chose one category or create one")
+    print("\nChose one category or create one")
     for c in category_data:
         print(c)
     category = input("Category: ").lower().strip()
@@ -43,8 +43,8 @@ def add_expense():
     data.append({"category":category, "amount":amount, "date":date, "time":time})
     save(data, EXPENSE_FILE)
 
-def monthly_expense():
-    month = input("Enter month (YYYY-MM): ")
+def monthly_summary():
+    month = input("\nEnter month (YYYY-MM): ")
     data = load(EXPENSE_FILE)
 
     total = 0
@@ -55,7 +55,7 @@ def monthly_expense():
     print("Total expense: ", total)
 
 def filter():
-    print("Filter by:\n1. Date\n2. Category")
+    print("\nFilter by:\n1. Date\n2. Category")
     choice = int(input("Enter your choice: "))
     
     match choice:
@@ -67,7 +67,7 @@ def filter():
             print("Invalid choice")
 
 def month_filter():
-    month = input("Enter month (YYYY-MM): ")
+    month = input("\nEnter month (YYYY-MM): ")
     data = load(EXPENSE_FILE)
     
     for exp in data:
@@ -75,10 +75,54 @@ def month_filter():
             print(f"{exp["category"]} | {exp["amount"]} | {exp["date"]}")
 
 def category_filter():
-    category = input("Enter category: ")
+    category = input("\nEnter category: ")
     data = load(EXPENSE_FILE)
 
     for exp in data:
         if exp["category"] == category:
             print(f"{exp["category"]} | {exp["amount"]} | {exp["date"]}")
 
+def view_expenses():
+    data = load(EXPENSE_FILE)
+    for exp in data:
+        print(f"{exp["category"]} | {exp["amount"]} | {exp["date"]}")
+
+def category_summary():
+    category = input("\nEnter category: ")
+    data = load(EXPENSE_FILE)
+
+    total = 0
+    for exp in data:
+        if exp["category"] == category:
+            total += exp["amount"]
+    print("Total expense: ", total)
+
+def main():
+    while True:
+        print("\n1. Add Expense")
+        print("2. View Expenses")
+        print("3. Filter")
+        print("4. Monthly Summary")
+        print("5. Category Summary")
+        print("6. Exit")
+
+        choice = int(input("Enter choice: "))
+
+        match choice:
+            case 1:
+                add_expense()
+            case 2:
+                view_expenses()
+            case 3:
+                filter()
+            case 4:
+                monthly_summary
+            case 5:
+                category_summary()
+            case 6:
+                print("Exiting...")
+                break
+            case _:
+                print("Invalid choice")
+
+main()
